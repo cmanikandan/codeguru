75292084ca00c32e22721075283516d57eed1ec8
Mani <mani.chandra@gmail.com>
2020-02-10 15:55:40 +0530

    Update NutritionFacts.java

--- NutritionFacts.java
+++ NutritionFacts.java
@@ -1,10 +1,13 @@
 package effectivejava.chapter2.item2.builder;
 
-// code cloned from https://github.com/cmanikandan/effective-java-3e-source-code/blob/master/src/effectivejava/chapter2/item2/builder/NutritionFacts.java
+// Code cloned from https://github.com/cmanikandan/effective-java-3e-source-code/blob/master/src/effectivejava/chapter2/item2/builder/NutritionFacts.java
 // Good code from the classic book - "Effective Java" by Joshua Bloch
 //
 // Builder Pattern  (Page 13)
+// 
+//
 public class NutritionFacts {
+    
     private final int servingSize;
     private final int servings;
     private final int calories;
